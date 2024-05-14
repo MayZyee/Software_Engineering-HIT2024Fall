9bdefd0500fc33dd4782016c63d8e40f226c4f12
Darl1ng <102397489+MayZyee@users.noreply.github.com>
2024-05-14 21:46:58 +0800

    Update main.cpp

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -3,14 +3,6 @@
 #include <sstream>
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> C4
-=======
-=======
->>>>>>> b000edef1180efd9a16eeae5e222f3e3362996bf
->>>>>>> master
     read file = read("input.txt");
     std::vector<std::string> words = file.processedLines;
     Graph graph(words);
